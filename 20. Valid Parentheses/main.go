c18f2d3bc422f038526128a3478d48e26e4e6a4b
Andrianata Daud <andrianta.321@gmail.com>
2023-08-03 19:14:19 +0700

    Create main.go

---
+++ 20. Valid Parentheses/main.go
@@ -0,0 +1,27 @@
+func isValid(s string) bool {
+	stack := make([]rune, 0)
+
+	for _, c := range s {
+		switch c {
+		case '(', '[', '{':
+			stack = append(stack, c)
+		case ')':
+			if len(stack) == 0 || stack[len(stack)-1] != '(' {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		case ']':
+			if len(stack) == 0 || stack[len(stack)-1] != '[' {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		case '}':
+			if len(stack) == 0 || stack[len(stack)-1] != '{' {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		}
+	}
+
+	return len(stack) == 0
+}
